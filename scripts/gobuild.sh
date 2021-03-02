fa15e7b267959fa7c2ff3f3711edc3b4a18cc166
Ankit Sanjay Timbadia <ankit.timbadia@PP-C02Z3254LVCG.local>
2021-03-02 16:35:06 +0530

    Adding systemd configuration for nixy, moving build scripts to dedicated directory

--- scripts/gobuild.sh
+++ scripts/gobuild.sh
@@ -4,10 +4,10 @@ export GOPATH=export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
-cd src/ && go build -o nixy
+cd ../src/ && go build -o nixy
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
 mv /usr/bin/nixy /usr/bin/nixy-$DATE_WITH_TIME
 ls -lath /usr/bin/nixy-$DATE_WITH_TIME
 mv nixy /usr/bin/nixy
 /usr/bin/nixy -v
-cp nginx.tmpl nixy.toml /etc/nixy
+cp nixy.toml /etc/nixy
