2f6a3609e24257584ebd38a7b52094b447bae4af
Vishnu Naini <vishnu.naini@phonepe.com>
2023-06-19 10:26:53 +0000

    use nplus in build name

--- scripts/gobuild.sh
+++ scripts/gobuild.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:$GOPATH/bin
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
 go mod tidy
-cd ../src/ && go build -ldflags="-X 'main.version=build-$(date +%Y%m%d%H%M%S)' -X 'main.date=$(date +"%Y-%m-%d %H:%M:%S")' -X 'main.commit=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse HEAD)'" -o nixy
+cd ../src/ && go build -ldflags="-X 'main.version=nplus-build-$(date +%Y%m%d%H%M%S)' -X 'main.date=$(date +"%Y-%m-%d %H:%M:%S")' -X 'main.commit=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse HEAD)'" -o nixy
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
 mv /usr/bin/nixy /usr/bin/nixy-$DATE_WITH_TIME
 ls -lath /usr/bin/nixy-$DATE_WITH_TIME
